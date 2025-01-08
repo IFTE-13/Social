ed526f3e9239536101c6b786ab8f03e599e1dd83
IFTE-13 <ifte.phoenix@gmail.com>
2025-01-08 23:57:50 +0600

    feat: add new navbar

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,7 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <>
-    <SignedOut>
-            <SignInButton mode="modal">
-              <Button>Sign In</Button>
-            </SignInButton>
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          <ModeToggle/>
+    hello
     </>
   );
 }
