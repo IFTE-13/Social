1ba59e70de4ed4eb41db1743c45f06c48d6efd22
IFTE-13 <ifte.phoenix@gmail.com>
2025-01-09 20:32:01 +0600

    feat: new prisma orm

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
-import ModeToggle from "@/components/mode-toggle";
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <>
